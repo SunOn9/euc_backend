e1f1c7f3771f921c4920db4607140c5c591ece39
Woka Tech <lehophattai@gmail.com>
2023-09-07 13:33:13 +0700

    commit

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, HttpCode, HttpStatus, Param } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { GrpcMethod } from '@nestjs/microservices';
 import { CreateAuthRequestDto } from './dto/create-auth.dto';
-import { AuthReply } from 'src/generated/auth/auth.reply';
+import { AuthListReply, AuthReply } from 'src/generated/auth/auth.reply';
 import { GetAuthConditionRequestDto } from './dto/get-condition-auth.dto';
 import CustomException from 'lib/utils/custom.exception';
 
@@ -10,22 +10,22 @@ import CustomException from 'lib/utils/custom.exception';
 export class AuthController {
   constructor(private readonly service: AuthService) {}
 
-  @GrpcMethod('AuthService', 'CreateAuth')
-  async createAuth(request: CreateAuthRequestDto): Promise<AuthReply> {
-    const response = {} as AuthReply;
-    const data = await this.service.create(request);
-    if (data.isErr()) {
-      throw new CustomException(
-        'ERROR',
-        data.error.message,
-        HttpStatus.BAD_REQUEST,
-      );
-    }
-    response.statusCode = 200;
-    response.message = 'success';
-    response.payload = data.value;
-    return response;
-  }
+  // @GrpcMethod('AuthService', 'CreateAuth')
+  // async createAuth(request: CreateAuthRequestDto): Promise<AuthReply> {
+  //   const response = {} as AuthReply;
+  //   const data = await this.service.create(request);
+  //   if (data.isErr()) {
+  //     throw new CustomException(
+  //       'ERROR',
+  //       data.error.message,
+  //       HttpStatus.BAD_REQUEST,
+  //     );
+  //   }
+  //   response.statusCode = 200;
+  //   response.message = 'success';
+  //   response.payload = data.value;
+  //   return response;
+  // }
 
   @GrpcMethod('AuthService', 'GetAuth')
   async getAuth(request: GetAuthConditionRequestDto): Promise<AuthReply> {
@@ -44,20 +44,20 @@ export class AuthController {
     return response;
   }
 
-  @Get('list')
-  async getListAuth(request: GetAuthConditionRequestDto): Promise<AuthReply> {
-    const response = {} as AuthReply;
-    const data = await this.service.getDetail(request);
-    if (data.isErr()) {
-      throw new CustomException(
-        'ERROR',
-        data.error.message,
-        HttpStatus.BAD_REQUEST,
-      );
-    }
-    response.statusCode = 200;
-    response.message = 'success';
-    response.payload = data.value;
-    return response;
-  }
+  // @Get('list')
+  // async getListAuth(request: GetAuthConditionRequestDto): Promise<AuthListReply> {
+  //   const response = {} as AuthReply;
+  //   const data = await this.service.(request);
+  //   if (data.isErr()) {
+  //     throw new CustomException(
+  //       'ERROR',
+  //       data.error.message,
+  //       HttpStatus.BAD_REQUEST,
+  //     );
+  //   }
+  //   response.statusCode = 200;
+  //   response.message = 'success';
+  //   response.payload = data.value;
+  //   return response;
+  // }
 }
