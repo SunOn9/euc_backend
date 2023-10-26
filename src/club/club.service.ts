089a5204d2cd21e73deb7b7aa544e7a8eab20d06
Woka Tech <lehophattai@gmail.com>
2023-10-26 11:01:08 +0700

    commut

--- src/club/club.service.ts
+++ src/club/club.service.ts
@@ -93,6 +93,7 @@ export class ClubService {
     return updateReply
   }
 
+  //TODO: soft delete club -> soft delete member-in-club too
   async remove(
     requestData: RemoveClubRequestDto,
     sessionId: string,
